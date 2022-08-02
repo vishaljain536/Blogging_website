7f7c975db727773a3b4960e385118abb2fe11ed3
Vishal <vishal.vj0123@gmail.com>
2022-08-03 00:36:47 +0530

    'blog'

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from.models import Post,Category,Author
 
 # Register your models here.
+
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Post)
